d9cf3e0d16c15a835d42a6d2fdb7ab524aa96e90
RamiroNoronha <100360738+RamiroNoronha@users.noreply.github.com>
2022-03-07 22:05:00 -0300

    Treinamento sobre o uso de Try e Catch em C#

---
+++ TreinandoTryCatchAlura/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace TreinandoTryCatchAlura
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Metodo();
+            }
+            catch (DivideByZeroException excecao)
+            {
+
+                Console.WriteLine(excecao.Message);
+                Console.WriteLine(excecao.StackTrace);
+            }
+            
+        }
+        public static int Dividir(int dividendo, int divisor)
+        {
+            return dividendo / divisor;
+        }
+
+        public static void Metodo()
+        {
+            TestarDivisao(0);
+        }
+
+        public static void TestarDivisao(int divisor)
+        {
+            Dividir(10, divisor);
+        }
+    }
+
+
+}
+
